e127ac7e28f2440067b22f2c41b48562e9ed8112
Rasmus Franke <rasmus.franke@gmail.com>
2017-04-14 15:49:35 +0200

    Generator updates

--- src/main/java/se/franke/fastmedian/Generator.java
+++ src/main/java/se/franke/fastmedian/Generator.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
+// Only for convenience and manual test utility
 public class Generator {
 
     public static void main(String[] args) throws IOException {
-        generateFile(Integer.valueOf(args[0]), Integer.valueOf(args[1]), Integer.valueOf(args[2]));
+        generateFile(Integer.valueOf(args[0]), Integer.valueOf(args[1]), Integer.valueOf(args[2]), args[3]);
     }
 
-    private static void generateFile(int size, int rangeMinInc, int rangeMaxInc) throws IOException {
-        FileWriter fileWriter = new FileWriter("values.txt");
+    private static void generateFile(final int size, final int rangeMinInc, final int rangeMaxInc, final String filename) throws IOException {
+        FileWriter fileWriter = new FileWriter(filename);
 
         IntStream stream = IntStream.generate(() -> ThreadLocalRandom.current().nextInt(rangeMinInc, rangeMaxInc + 1));
         stream.limit(size).forEach(i -> {
